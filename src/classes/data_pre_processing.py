9a081e31b033a71e1c33ef494bd7c4ee6c8f814a
Ruohan Wang <ruohanwang@20gmail.com>
2024-02-15 17:07:18 +0100

    function moving filtering without lag added

--- src/classes/data_pre_processing.py
+++ src/classes/data_pre_processing.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 import os
 import pathlib
+import matplotlib.pyplot as plt
 
 
 def load_csv(method):
@@ -49,3 +51,45 @@ def get_classification_cols(df_data):
     y = df_data[['whether he/she donated blood in March 2007']]
 
     return X, y
+
+
+def centered_moving_average(data, window_size):
+    """
+    Example: data2 = centered_moving_average(data, 50)
+
+    :param data: Dataframe, with multiple columns
+    :param window_size: Integer, recommend range (20-50)
+    :return: data_moving_filtering Dataframe, using moving filter to smooth the data,
+        having the same structure as the input data
+    """
+    # data2 is an empty Dataframe with the same structure as the input data
+    data_moving_filtering = data.reindex(columns=data.columns).iloc[0:0]
+    data_moving_filtering['Datum'] = data['Datum']
+    half_window = window_size // 2
+    smoothed_data = np.zeros(len(data))
+
+    for column in data:
+        if column == 'Datum':
+            pass
+        else:
+            for i in range(0, len(smoothed_data)):
+                if i <= half_window + 1:
+                    smoothed_data[i] = np.mean(data[column][0:i + half_window])
+                else:
+                    smoothed_data[i] = np.mean(data[column][i - half_window:i + half_window])
+
+            data_moving_filtering[column] = smoothed_data
+
+    return data_moving_filtering
+
+
+# data = load_csv('regression')
+# data2 = centered_moving_average(data, 50)
+# print(data.head())
+# print(data2.head())
+#
+# for column in data:
+#     plt.plot(data[column], label='Original Data')
+#     plt.plot(data2[column], label='Filtering Data')
+#     plt.legend()
+#     plt.show()
