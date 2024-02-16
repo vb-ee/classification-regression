35e2f82815d8a668c4c2d6c42c1d17ac9f63e492
Vekil Bekmyradov <bekmuradovimprove@gmail.com>
2024-02-16 17:33:17 +0100

    refactor: wrap data processing logic into class

--- utils/constants.py
+++ utils/constants.py
@@ -1,17 +1,22 @@
 from enum import Enum
 
 
-# Defined Enum class for data paths to avoid hardcoded variables
 class DATA_PATH(Enum):
+    # Define Enum class for data paths to avoid hardcoded in code
+
     REGRESSION_RAW = ['data', 'raw', 'regression', 'regression.csv']
     CLASSIFICATION_RAW = ['data', 'raw', 'classification', 'transfusion.csv']
-    REGRESSION_PROCESSED = ['data', 'processed', 'regression', 'regression.csv']
-    CLASSIFICATION_PROCESSED = ['data', 'processed', 'classification', 'transfusion.csv']
+    REGRESSION_PROCESSED = ['data', 'processed',
+                            'regression', 'regression.csv']
+    CLASSIFICATION_PROCESSED = ['data', 'processed',
+                                'classification', 'transfusion.csv']
 
 
-class METHOD(Enum):
+class MODEL_FEATURE(Enum):
+    # Define Enum class for model features to avoid hardcoded values
+
     REGRESSION_INPUT = ['Methangehalt CH4', 'TS-Wert', 'pH-Wert']
     REGRESSION_OUTPUT = ['BHKW1_Biogas', 'BHKW2_Biogas']
-    CLASSIFICATION_INPUT = ['Recency (months)', 'Frequency (times)', 'Monetary (c.c. blood)', 'Time (months)']
+    CLASSIFICATION_INPUT = [
+        'Recency (months)', 'Frequency (times)', 'Monetary (c.c. blood)', 'Time (months)']
     CLASSIFICATION_OUTPUT = ['whether he/she donated blood in March 2007']
-
--- utils/data_pre_processing.py
+++ utils/data_pre_processing.py
@@ -8,59 +8,60 @@ import numpy as np
 
 
 # custom imports
-from constants import DATA_PATH, METHOD
+from constants import MODEL_FEATURE
 
 
-def load_csv(filepath: DATA_PATH):
-    """
-    Example: data = load_csv(DATA_PATH.CLASSIFICATION_RAW)
+class DataProcess:
+    '''
+    class for data processing
 
-    :return: Dataframe
-    """
-    return pd.read_csv(join(dirname(dirname(__file__)), *filepath.value))
+    pass DATA_PATH enum as an argument when creatin an object
 
+    ex: data_process = DataProcess(DATA_PATH.REGRESSION_RAW)
+    '''
 
-def get_cols(df_data, method: METHOD):
-    """
-    Example: df_cols = get_cols(data, METHOD.REGRESSION_INPUT)
+    def __init__(self, filepath):
+        self.filepath = join(dirname(dirname(__file__)), *filepath.value)
+        self.data = pd.read_csv(self.filepath)
 
-    :param method: Enum, choose the columns u want to get
-    :param df_data: Dataframe, get the data from load_csv function
-    :return: X: Dataframe
-    """
-    X = df_data.reindex(columns=method.value).iloc[0:0]
-    for col in method.value:
-        X[[col]] = df_data[[col]]
+    def get_columns(data, model_features: MODEL_FEATURE):
+        '''
+        Example: df_cols = get_cols(data, MODEL_FEATURE.REGRESSION_INPUT)
 
-    return X
+        :param model_features: Enum, choose the columns u want to get
+        :param data: Dataframe, get the data from pandas read_csv function
+        :return: X: Dataframe
+        '''
+        return data[model_features.value]
 
+    # TODO: refactor the function, avoid using reindex and deal with hardcoded variables
+    def centered_moving_average(data, window_size):
+        '''
+        Example: data2 = centered_moving_average(data, 50)
 
-def centered_moving_average(data, window_size):
-    """
-    Example: data2 = centered_moving_average(data, 50)
+        :param data: Dataframe, with multiple columns
+        :param window_size: Integer, recommend range (20-50)
+        :return: data_moving_filtering Dataframe, using moving filter to smooth the data,
+            having the same structure as the input data
+        '''
+        # data2 is an empty Dataframe with the same structure as the input data
+        data_moving_filtering = data.reindex(columns=data.columns).iloc[0:0]
+        data_moving_filtering['Datum'] = data['Datum']
+        half_window = window_size // 2
+        smoothed_data = np.zeros(len(data))
 
-    :param data: Dataframe, with multiple columns
-    :param window_size: Integer, recommend range (20-50)
-    :return: data_moving_filtering Dataframe, using moving filter to smooth the data,
-        having the same structure as the input data
-    """
-    # data2 is an empty Dataframe with the same structure as the input data
-    data_moving_filtering = data.reindex(columns=data.columns).iloc[0:0]
-    data_moving_filtering['Datum'] = data['Datum']
-    half_window = window_size // 2
-    smoothed_data = np.zeros(len(data))
+        for column in data:
+            if column == 'Datum':
+                pass
+            else:
+                for i in range(0, len(smoothed_data)):
+                    if i <= half_window + 1:
+                        smoothed_data[i] = np.mean(
+                            data[column][0:i + half_window])
+                    else:
+                        smoothed_data[i] = np.mean(
+                            data[column][i - half_window:i + half_window])
 
-    for column in data:
-        if column == 'Datum':
-            pass
-        else:
-            for i in range(0, len(smoothed_data)):
-                if i <= half_window + 1:
-                    smoothed_data[i] = np.mean(data[column][0:i + half_window])
-                else:
-                    smoothed_data[i] = np.mean(
-                        data[column][i - half_window:i + half_window])
+                data_moving_filtering[column] = smoothed_data
 
-            data_moving_filtering[column] = smoothed_data
-
-    return data_moving_filtering
+        return data_moving_filtering
