27c815d6a350ec0a337ac5701669d319154174a3
Ruohan Wang <ruohanwang@20gmail.com>
2024-02-17 10:16:55 +0100

    refactor filtering model, add window size range

--- utils/data_pre_processing.py
+++ utils/data_pre_processing.py
@@ -1,67 +1,68 @@
 # import built-in packages
 from os.path import dirname, join
 
+import numpy as np
 # import third party packages
 import pandas as pd
-import numpy as np
+
+# custom imports
+from constants import MODEL_FEATURE, DATA_PATH
+
+
 # import matplotlib.pyplot as plt
 
 
-# custom imports
-from constants import MODEL_FEATURE
-
-
 class DataProcess:
-    '''
+    """
     class for data processing
 
-    pass DATA_PATH enum as an argument when creatin an object
+    pass DATA_PATH enum as an argument when create an object
 
     ex: data_process = DataProcess(DATA_PATH.REGRESSION_RAW)
-    '''
+    """
 
     def __init__(self, filepath):
         self.filepath = join(dirname(dirname(__file__)), *filepath.value)
         self.data = pd.read_csv(self.filepath)
 
-    def get_columns(data, model_features: MODEL_FEATURE):
-        '''
+    def get_columns(self, model_features: MODEL_FEATURE):
+        """
         Example: df_cols = get_cols(data, MODEL_FEATURE.REGRESSION_INPUT)
 
         :param model_features: Enum, choose the columns u want to get
-        :param data: Dataframe, get the data from pandas read_csv function
         :return: X: Dataframe
-        '''
-        return data[model_features.value]
+        """
 
-    # TODO: refactor the function, avoid using reindex and deal with hardcoded variables
-    def centered_moving_average(data, window_size):
-        '''
+        return self.data[model_features.value]
+
+    def centered_moving_average(self, data, window_size):
+        """
+        It's only used for regression model.
+        It shouldn't be used for classification model.
         Example: data2 = centered_moving_average(data, 50)
 
         :param data: Dataframe, with multiple columns
         :param window_size: Integer, recommend range (20-50)
         :return: data_moving_filtering Dataframe, using moving filter to smooth the data,
             having the same structure as the input data
-        '''
-        # data2 is an empty Dataframe with the same structure as the input data
-        data_moving_filtering = data.reindex(columns=data.columns).iloc[0:0]
-        data_moving_filtering['Datum'] = data['Datum']
+        """
+        if window_size not in range(20, 51):
+            raise ValueError('Window size should in range 20-50')
+
         half_window = window_size // 2
-        smoothed_data = np.zeros(len(data))
+        for column in data.columns:
+            temp = []
+            for i in range(0, len(data)):
+                if i <= half_window + 1:
+                    temp.append(np.mean(data[column][0:i + half_window]))
+                else:
+                    temp.append(np.mean(data[column][i - half_window:i + half_window]))
+            data.loc[:, column] = temp
 
-        for column in data:
-            if column == 'Datum':
-                pass
-            else:
-                for i in range(0, len(smoothed_data)):
-                    if i <= half_window + 1:
-                        smoothed_data[i] = np.mean(
-                            data[column][0:i + half_window])
-                    else:
-                        smoothed_data[i] = np.mean(
-                            data[column][i - half_window:i + half_window])
+        return data
 
-                data_moving_filtering[column] = smoothed_data
 
-        return data_moving_filtering
+data_process = DataProcess(DATA_PATH.REGRESSION_RAW)
+X = data_process.get_columns(MODEL_FEATURE.REGRESSION_INPUT)
+X1 = data_process.centered_moving_average(X, 20)
+print(X1)
