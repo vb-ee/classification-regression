1941e0afeb377741bd15a46b582ce1ecb137d403
Ruohan Wang <ruohanwang@20gmail.com>
2024-02-29 14:46:03 +0100

    add replace_outlier() in data_pre_processing.py

--- src/utils/data_pre_processing.py
+++ src/utils/data_pre_processing.py
@@ -1,5 +1,5 @@
 # import built-in packages
-from os.path import dirname, join
+from datetime import datetime, timedelta
 
 # import third party packages
 import numpy as np
@@ -7,83 +7,98 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 
-class DataProcess:
-    """
-    class for data processing
+def centered_moving_average(data, model_features: list[str], window_size: int = 20) -> pd.DataFrame:
+    '''
+    use moving filter to smooth the data and return the filtered data
+    It's recommended only used for regression model.
 
-    pass DATA_PATH enum as an argument when create an object
+    :param data: Dataframe
+    :param window_size: Integer, recommend range (20-50)
+    :param model_features: List of strings, which are the columns' names of the data
+    :return: Dataframe
 
-    ex: data_process = DataProcess(DATA_PATH.REGRESSION_RAW.value)
-    """
+    ex: filtered_data = regression_data.centered_moving_average(20)
+    '''
 
-    def __init__(self, filepath: list[str]):
-        self.filepath = join(
-            dirname(dirname(dirname(__file__))), *filepath)
-        self.data = pd.read_csv(self.filepath)
+    if window_size not in range(20, 51):
+        raise ValueError('Window size should in range 20-50')
 
-    def replace_outlier(self, model_features: list[str], threshold: float = 1.5):
-        """
-        replace the outliers with maximum or minimum value,
-        this function will change the original dataframe
+    output = data[model_features]
+    filtered_data = pd.DataFrame()
+    half_window = window_size // 2
+    for column in output:
+        temp = []
+        for i in range(0, len(output)):
+            if i <= half_window + 1:
+                temp.append(np.mean(output[column][0:i + half_window]))
+            else:
+                temp.append(
+                    np.mean(output[column][i - half_window:i + half_window]))
+        filtered_data[column] = temp
 
-        :param threshold: Float, recommend range 1.0-3.0
-        :param model_features: List of strings, which are the columns' names of the data
+    return filtered_data
 
-        ex: df_regression.replace_outlier(MODEL_FEATURE.REGRESSION_INPUT.value)
-        """
-        if threshold > 3.0 or threshold < 1.0:
-            raise ValueError('threshold should in range 1.0-3.0')
 
-        for col_name in self.data[model_features]:
-            quartile_1, quartile_3 = np.percentile(self.data[col_name], [25, 75])
-            iqr = quartile_3 - quartile_1
-            for idx in self.data.index:
-                if self.data[col_name][idx] < quartile_1 - threshold * iqr:
-                    self.data.loc[idx, col_name] = quartile_1 - threshold * iqr
-                elif self.data[col_name][idx] > quartile_3 + threshold * iqr:
-                    self.data.loc[idx, col_name] = quartile_3 + threshold * iqr
+def standard_scaling(data, model_features: list[str]) -> pd.DataFrame:
+    '''
+    It's recommended only used for classification model.
 
-    def centered_moving_average(self, model_features: list[str], window_size: int = 85):
-        """
-        use moving filter to smooth the data and return the filtered data
-        It's recommended only used for regression model.
+    :param: data: Dataframe
+    :return: scaled_data: Dataframe, which can be used for coming classification
 
-        :param window_size: Integer, recommend range (20-50)
-        :param model_features: List of strings, which are the columns' names of the data
-        :return: Dataframe
+    ex: scaled_data = classification_data.standard_scaling()
+    '''
+    output = data[model_features]
+    scaler = StandardScaler()
+    scaled_data = pd.DataFrame(scaler.fit_transform(output))
+    scaled_data.columns = output.columns
 
-        ex: filtered_data = regression_data.centered_moving_average(20)
-        """
+    return scaled_data
 
-        if window_size not in range(20, 101):
-            raise ValueError('Window size should in range 20-100')
 
-        output = self.data[model_features]
-        filtered_data = pd.DataFrame()
-        half_window = window_size // 2
-        for column in output:
-            temp = []
-            for i in range(0, len(output)):
-                if i <= half_window + 1:
-                    temp.append(np.mean(output[column][0:i + half_window]))
-                else:
-                    temp.append(
-                        np.mean(output[column][i - half_window:i + half_window]))
-            filtered_data[column] = temp
+def matlab_time_to_datetime(date_column: pd.DataFrame) -> list[str]:
+    '''
+    convert matlab date time column to readable time
 
-        return filtered_data
+    param: data: Dataframe
+    '''
 
-    def standard_scaling(self, model_features: list[str]):
-        """
-        It's recommended only used for classification model.
+    start_date = datetime(1, 1, 1)
+    dates = []
 
-        :return: scaled_data: Dataframe, which can be used for coming classification
+    for date_decimal in date_column:
+        # Convert decimal to datetime object
+        delta = timedelta(days=date_decimal)
 
-        ex: scaled_data = classification_data.standard_scaling()
-        """
-        output = self.data[model_features]
-        scaler = StandardScaler()
-        scaled_data = pd.DataFrame(scaler.fit_transform(output))
-        scaled_data.columns = output.columns
+        date = start_date + delta
 
-        return scaled_data
+        # Output: 2003-01-01
+        dates.append(date.strftime('%Y-%m-%d %H'))
+
+    return dates
+
+
+def replace_outlier(data, model_features: list[str], threshold: float = 1.5) -> pd.DataFrame:
+    '''
+    replace the outliers with maximum or minimum value,
+    this function will change the original dataframe
+
+    :param data: Dataframe
+    :param threshold: Float, recommend range 1.0-3.0
+    :param model_features: List of strings, which are the columns' names of the data
+
+    ex: df_regression.replace_outlier(MODEL_FEATURE.REGRESSION_INPUT.value)
+    '''
+    if threshold > 3.0 or threshold < 1.0:
+        raise ValueError('threshold should in range 1.0-3.0')
+
+    for col_name in data[model_features]:
+        quartile_1, quartile_3 = np.percentile(data[col_name], [25, 75])
+        iqr = quartile_3 - quartile_1
+        for idx in data.index:
+            if data[col_name][idx] < quartile_1 - threshold * iqr:
+                data.loc[idx, col_name] = quartile_1 - threshold * iqr
+            elif data[col_name][idx] > quartile_3 + threshold * iqr:
+                data.loc[idx, col_name] = quartile_3 + threshold * iqr
+
+    return data
