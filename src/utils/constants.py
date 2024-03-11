f3357e7024da6a7faee537cef1f8f4472339e6f3
Szuyu-Liu <szuyuliu41@gmail.com>
2024-03-11 20:20:19 +0800

    Add the classification kernels

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -41,4 +41,4 @@ class MODEL_FEATURE(Enum):
 
 
 # Define array for classification kernels to avoid hardcoded values. Add more kernels here if needed
-CLASSIFICATION_KERNELS = ['Linear', 'SVM']
+CLASSIFICATION_KERNELS = ['linear', 'rbf', 'sigmoid', 'poly']
